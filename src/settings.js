000fc2496d7dc0188a262077850e1f0c522be96e
ThelsK <github@thels.nl>
2019-10-10 00:14:14 +0200

    Set Heroku websocket to secure

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 // export const baseUrl = "ws://localhost:6789"
-export const baseUrl = "ws://lit-dawn-24467.herokuapp.com"
+export const baseUrl = "wss://lit-dawn-24467.herokuapp.com"
 
 export const messageTimeout = 8000
