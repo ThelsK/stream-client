3c763aaea5869ee1b48f914827ec1b99fbdcce99
ThelsK <github@thels.nl>
2019-10-10 15:10:08 +0200

    Add stop button to Watch

--- src/views/Watch/index.js
+++ src/views/Watch/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import { connect } from "react-redux"
 
 import Connection from "../Connection"
+import { clearHostdevice } from "../../actions/hostdevice"
+import { clearStatus } from "../../actions/status"
 
 class Component extends React.Component {
   videoRef = React.createRef()
@@ -14,10 +16,20 @@ class Component extends React.Component {
     this.videoRef.current.srcObject = this.props.stream
   }
 
+  stopWatching = () => {
+    this.props.webSocket.send(JSON.stringify({
+      type: "status", status: "",
+    }))
+    this.props.clearHostdevice()
+    this.props.clearStatus()
+  }
+
   render() {
     return <div className="column">
-      <Connection />
       <h2 className="textlarge">Watching {this.props.hostdevice}</h2>
+      <button className="button" onClick={this.stopWatching}
+      >Stop</button>
+      <Connection />
       <video className="video" ref={this.videoRef} autoPlay controls />
     </div>
   }
@@ -29,6 +41,6 @@ const mapStateToProps = state => ({
   stream: state.stream,
 })
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = { clearHostdevice, clearStatus }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component)
