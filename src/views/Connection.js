8114b15abecd06792860c1ca719bcd0d5d525d11
ThelsK <github@thels.nl>
2019-10-10 02:05:01 +0200

    Fixed oneway stream

--- src/views/Connection.js
+++ src/views/Connection.js
@@ -2,6 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 
 import { clearSignal } from "../actions/signal"
+import { setStream } from "../actions/stream"
 
 class Component extends React.Component {
   connections = []
@@ -13,14 +14,28 @@ class Component extends React.Component {
       const connection = new RTCPeerConnection({
         iceServers: [{
           urls: [
-            "stun:stun3.l.google.com:19302",
-            "stun:stun4.l.google.com:19302",
+            "stun:stun3.1.google.com:19302",
+            "stun:stun4.1.google.com:19302",
           ],
         },],
       })
 
       // Store the linked username in the connection.
       connection.otherDevicename = this.props.hostdevice
+
+      // Handle incoming stream.
+      connection.onaddstream = event => {
+        console.log("Add Stream:", event)
+        this.props.setStream(event.stream)
+      }
+
+      // Send outgoing stream.
+      // connection.addStream(this.props.stream)
+
+      // Handle ICE candidates.
+      connection.onicecandidate = event => {
+        console.log("ICE Candidate:", event)
+      }
 
       // Establish a connection offer.
       connection.createOffer(offer => {
@@ -29,18 +44,7 @@ class Component extends React.Component {
           type: "target", target: connection.otherDevicename,
           payload: { type: "offer", offer }
         }))
-      }, console.error)
-
-      // Handle incoming stream.
-      connection.onaddstream = event => {
-        console.log("Add Stream:", event)
-        this.props.setStream(event.stream)
-      }
-
-      // Handle ICE candidates.
-      connection.onicecandidate = event => {
-        console.log("ICE Candidate:", event)
-      }
+      }, console.error, { offerToReceiveVideo: true })
 
       // Add the connection to the list of connections.
       this.connections.push(connection)
@@ -48,72 +52,87 @@ class Component extends React.Component {
   }
 
   componentDidUpdate() {
-    if (this.props.signal.type) {
-      console.log("Signal found.")
+    if (!this.props.signal.type) {
+      return
+    }
+    console.log("Signal found.")
 
-      // Find the connection to this device.
-      let connection = this.connections.find(conn =>
-        conn.otherDevicename === this.props.signal.senderDevicename)
+    // Find the connection to this device.
+    let connection = this.connections.find(conn =>
+      conn.otherDevicename === this.props.signal.senderDevicename)
 
-      // If no connection exists, start a new connection.
-      if (!connection) {
-        connection = new RTCPeerConnection({
-          iceServers: [{
-            urls: [
-              "stun:stun3.l.google.com:19302",
-              "stun:stun4.l.google.com:19302",
-            ],
-          },],
-        })
+    // If no connection exists, start a new connection.
+    if (!connection) {
+      connection = new RTCPeerConnection({
+        iceServers: [{
+          urls: [
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+          ],
+        },],
+      })
 
-        // Store the linked username in the connection.
-        connection.otherDevicename = this.props.signal.senderDevicename
+      // Store the linked username in the connection.
+      connection.otherDevicename = this.props.signal.senderDevicename
 
-        // Send outgoing stream.
-        connection.addStream(this.props.stream)
+      // Handle incoming stream.
+      connection.onaddstream = event => {
+        console.log("Add Stream:", event)
+        this.props.setStream(event.stream)
+      }
 
-        // Handle ICE candidates.
-        connection.onicecandidate = event => {
-          console.log("ICE Candidate:", event)
+      // Send outgoing stream.
+      connection.addStream(this.props.stream)
+
+      // Handle ICE candidates.
+      connection.onicecandidate = event => {
+        console.log("ICE Candidate:", event)
+        if (event.candidate) {
+          this.props.webSocket.send(JSON.stringify({
+            type: "target", target: connection.otherDevicename,
+            payload: { type: "candidate", candidate: event.candidate }
+          }))
         }
-
-        // Add the connection to the list of connections.
-        this.connections.push(connection)
       }
 
-      switch (this.props.signal.type) {
+      // Add the connection to the list of connections.
+      this.connections.push(connection)
+    }
 
-        case "offer":
-          console.log("Received offer")
-          connection.setRemoteDescription(new RTCSessionDescription(
-            this.props.signal.offer))
-          connection.createAnswer(answer => {
-            connection.setLocalDescription(answer)
-            this.props.webSocket.send(JSON.stringify({
-              type: "target", target: connection.otherDevicename,
-              payload: { type: "answer", answer }
-            }))
-          }, console.error)
-          this.props.clearSignal()
-          return
+    switch (this.props.signal.type) {
 
-        case "answer":
-          console.log("Received answer")
-          connection.setRemoteDescription(new RTCSessionDescription(
-            this.props.signal.answer))
-          this.props.clearSignal()
-          return
+      case "offer":
+        console.log("Received offer")
+        connection.setRemoteDescription(new RTCSessionDescription(
+          this.props.signal.offer))
+        connection.createAnswer(answer => {
+          connection.setLocalDescription(answer)
+          this.props.webSocket.send(JSON.stringify({
+            type: "target", target: connection.otherDevicename,
+            payload: { type: "answer", answer }
+          }))
+        }, console.error)
+        this.props.clearSignal()
+        console.log("Connection:", connection)
+        return
 
-        case "candidate":
-          connection.addIceCandidate(new RTCIceCandidate(
-            this.props.signal.candidate))
-          this.props.clearSignal()
-          return
+      case "answer":
+        console.log("Received answer")
+        connection.setRemoteDescription(new RTCSessionDescription(
+          this.props.signal.answer))
+        this.props.clearSignal()
+        console.log("Connection:", connection)
+        return
 
-        default:
-          console.error("Unknown data:", this.props.signal)
-          this.props.clearSignal()
-      }
+      case "candidate":
+        connection.addIceCandidate(new RTCIceCandidate(
+          this.props.signal.candidate))
+        this.props.clearSignal()
+        return
+
+      default:
+        console.error("Unknown data:", this.props.signal)
+        this.props.clearSignal()
     }
   }
 
@@ -129,6 +148,6 @@ const mapStateToProps = state => ({
   signal: state.signal,
 })
 
-const mapDispatchToProps = { clearSignal }
+const mapDispatchToProps = { clearSignal, setStream }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component)
